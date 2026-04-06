b83025e12245c87a82d424aaadc625eeeed82b4d
tomech <109527747+tomech4@users.noreply.github.com>
2026-04-06 23:47:12 +0200

    add str representation and ordering for Order model

--- db/models.py
+++ db/models.py
@@ -60,3 +60,9 @@ class MovieSession(models.Model):
 class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="user_orders")
+
+    def __str__(self):
+        return self.created_at
+
+    class Meta:
+        ordering = ["-created_at"]
