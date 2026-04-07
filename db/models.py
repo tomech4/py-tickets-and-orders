9b66650908b039efbf7d3f9c4637de36d405f1a0
tomech <109527747+tomech4@users.noreply.github.com>
2026-04-07 12:28:13 +0200

    add clean, save methods and constraints to Ticket model

--- db/models.py
+++ db/models.py
@@ -76,3 +76,16 @@ class Ticket(models.Model):
 
     def __str__(self):
         return f"{self.movie_session.movie.title} {self.movie_session.show_time} (row: {self.row}, seat: {self.seat})"
+
+    def clean(self):
+        if self.row > self.movie_session.cinema_hall.rows or self.seat > self.movie_session.cinema_hall.seats_in_row:
+            raise ValidationError(f"Invalid seat/row value(s), max values allowed: seat - {self.movie_session.cinema_hall.seats_in_row}, row - {self.movie_session.cinema_hall.rows}; provided seat - {self.seat}, row - {self.row}")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=["row", "seat", "movie_session"])
+        ]
