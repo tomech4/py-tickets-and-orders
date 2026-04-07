bb3b2f864e12df0ba04201127ae19dd71b0597ad
tomech <109527747+tomech4@users.noreply.github.com>
2026-04-07 21:07:44 +0200

    create user service

---
+++ services/user.py
@@ -0,0 +1,42 @@
+from db.models import User
+
+
+def create_user(
+    username: str,
+    password: str,
+    email: str = None,
+    first_name: str = None,
+    last_name: str = None
+):
+    return User.objects.create_user(
+        username=username,
+        password=password,
+        email=email,
+        first_name=first_name,
+        last_name=last_name
+    )
+
+def get_user(user_id: int):
+    return User.objects.get(user_id=user_id)
+
+def update_user(
+    user_id: int,
+    username: str = None,
+    password: str = None,
+    email: str = None,
+    first_name: str = None,
+    last_name: str = None
+):
+    user = get_user(user_id)
+    if username:
+        user.username = username
+    if password:
+        user.set_password(password)
+    if email:
+        user.email = email
+    if first_name:
+        user.first_name = first_name
+    if last_name:
+        user.last_name = last_name
+
+    user.save()
