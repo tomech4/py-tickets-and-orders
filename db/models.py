9d595c954e1db9aeeecdb75c9c82c6c997e46f20
tomech <109527747+tomech4@users.noreply.github.com>
2026-04-07 20:09:36 +0200

    add django User model

--- db/models.py
+++ db/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models.constraints import UniqueConstraint
