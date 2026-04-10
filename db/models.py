3c682b55f16b936f6a3410e5f0ab9e512b398b73
tomech <109527747+tomech4@users.noreply.github.com>
2026-04-10 21:21:09 +0200

    revert string representation to a previous version to match tests

--- db/models.py
+++ db/models.py
@@ -71,7 +71,7 @@ class Order(models.Model):
     )
 
     def __str__(self) -> str:
-        return f'<Order: {self.created_at.strftime("%Y-%m-%d %H:%M:%S")}>'
+        return self.created_at.strftime("%Y-%m-%d %H:%M:%S")
 
     class Meta:
         ordering = ["-created_at"]
@@ -87,9 +87,8 @@ class Ticket(models.Model):
 
     def __str__(self) -> str:
         return (
-            "<Ticket: "
             f"{self.movie_session.movie.title} {self.movie_session.show_time} "
-            f"(row: {self.row}, seat: {self.seat})>"
+            f"(row: {self.row}, seat: {self.seat})"
         )
 
     def clean(self) -> None:
